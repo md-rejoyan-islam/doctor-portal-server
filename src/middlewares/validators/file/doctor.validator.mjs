c98d18ca3cb90c84c67d06e6c602fd6ad4aa7a38
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-05 21:11:25 +0600

    doctor middleware validator add

---
+++ src/middlewares/validators/file/doctor.validator.mjs
@@ -0,0 +1,61 @@
+import { body } from "express-validator";
+import createError from "http-errors";
+
+// create doctor validator
+export const doctorCreateValidator = [
+  body("name")
+    .notEmpty()
+    .withMessage("Doctor name is required")
+    .trim()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters long"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email"),
+  body("photo")
+    .notEmpty()
+    .withMessage("Photo is required")
+    .isURL()
+    .withMessage("Invalid photo url"),
+  body("specialty")
+    .notEmpty()
+    .withMessage("Specialty is required")
+    .trim()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Specialty must be at least 3 characters long"),
+  body("degree")
+    .notEmpty()
+    .withMessage("Degree is required")
+    .trim()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Degree must be at least 3 characters long"),
+];
+
+// update doctor validator
+export const doctorUpdateValidator = [
+  body("name")
+    .optional()
+    .trim()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters long"),
+  body("email").optional().isEmail().withMessage("Invalid email"),
+  body("photo").optional().isURL().withMessage("Invalid photo url"),
+  body("specialty")
+    .optional()
+    .trim()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Specialty must be at least 3 characters long"),
+  body("degree")
+    .optional()
+    .trim()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Degree must be at least 3 characters long"),
+];
