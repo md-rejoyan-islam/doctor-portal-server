36bdd9d6271e4ac5af7bdb33af1cd089ac88e6e0
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-05 21:18:36 +0600

    feedback document write

--- src/v1/controllers/feedback.controllers.mjs
+++ src/v1/controllers/feedback.controllers.mjs
@@ -4,7 +4,22 @@ import { successResponse } from "../../helper/responseHandler.mjs";
 import checkMongoID from "../../helper/checkMongoId.mjs";
 import createError from "http-errors";
 
-// get all feedbacks
+/**
+ *
+ * @description    Get All Feedbacks
+ * @apiMethod      GET
+ *
+ * @apiRoute       /api/v1/feedbacks
+ * @apiAccess      Private
+ *
+ * @apiCookie      { accessToken }
+ *
+ * @apiQuery       [ email = string ]
+ *
+ * @apiSuccess     { success : true , message , data }
+ * @apiFailed      { success : false, error : { status : code , message} }
+ * @apiError       ( Not Found 404 )  Could not find any feedbacks.
+ */
 export const getAllFeedbacks = asyncHandler(async (req, res) => {
   const email = req.query.email;
 
@@ -28,7 +43,19 @@ export const getAllFeedbacks = asyncHandler(async (req, res) => {
   });
 });
 
-// get feedback by id
+/**
+ * @description       Get feedback by id
+ * @apiMethod         GET
+ *
+ * @apiRoute          /api/v1/feedbacks/:id
+ * @apiAccess         Private
+ *
+ * @apiParams         [ id = string ]     required
+ *
+ * @apiSuccess        { success : true , message , data }
+ * @apiFailed         { success : false, error : { status : code , message} }
+ * @apiError          ( Not Found 404 )   Could not find any feedback.
+ */
 export const getFeedbackById = asyncHandler(async (req, res) => {
   checkMongoID(req.params.id);
 
@@ -47,7 +74,19 @@ export const getFeedbackById = asyncHandler(async (req, res) => {
   });
 });
 
-// create feedback
+/**
+ * @description       Create feedback
+ * @apiMethod         POST
+ *
+ * @apiRoute          /api/v1/feedbacks
+ * @apiAccess         Public
+ *
+ * @apiBody           { name : string , email : string , message : string }
+ *
+ * @apiSuccess        { success : true , message , data }
+ * @apiFailed         { success : false, error : { status : code , message} }
+ * @apiError          ( Bad Request 400 )   Please provide all the required fields
+ */
 export const createFeedback = asyncHandler(async (req, res) => {
   const { name, email, message } = req.body;
 
@@ -66,7 +105,21 @@ export const createFeedback = asyncHandler(async (req, res) => {
   });
 });
 
-// delete feedback by id
+/**
+ * @description       Delete feedback by id
+ * @apiMethod         DELETE
+ *
+ * @apiRoute          /api/v1/feedbacks/:id
+ *
+ * @apiAccess         Private
+ *
+ * @apiParams         [ id = string ]     required
+ *
+ * @apiSuccess        { success : true , message , data }
+ * @apiFailed         { success : false, error : { status : code , message} }
+ * @apiError          ( Not Found 404 )   Could not find any feedback.
+ *
+ */
 export const deleteFeedbackById = asyncHandler(async (req, res) => {
   checkMongoID(req.params.id);
 
