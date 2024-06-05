14e9ce016e52914cb68f467ab6959389342509f9
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-05 22:12:20 +0600

    doctos seed controller added

--- data/seeds.doctor.mjs
+++ data/seeds.doctor.mjs
@@ -1,3 +1,76 @@
-const data = [];
+const data = [
+  {
+    name: "Dr. Ashis Kumar Biswas",
+    email: "ashish.komar@gmail.com",
+    specialty: "Teeth Cleaning",
+    degree: "BDS, MCPS (Dental Surgery)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717579318/doctor-portal/doctors/1717580306834-photo_xapugc.jpg",
+  },
+  {
+    name: "Dr. Lutfun Nahar",
+    email: "lutfun@hotmail.com",
+    specialty: "Teeth Cleaning",
+    degree: "BDS, MSc (Singapore)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717579406/doctor-portal/doctors/1717580075219-photo_gi1pvi.jpg",
+  },
+  {
+    name: "Prof. Dr. Mostaque H Sattar",
+    email: "dr.mostaque@gmail.com",
+    specialty: "Teeth Cleaning",
+    degree: "BDS, DDS, MCPS, FICCDE, FICD",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717579544/doctor-portal/doctors/1717579824265-photo_brrg7t.jpg",
+  },
+  {
+    name: "Dr. Suryaprakash Sharma",
+    email: "suryaprakash@gmail.com",
+    specialty: "Cosmetic Dentistry",
+    degree: "BDS MDS (Prosthodontics)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717579900/doctor-portal/doctors/1717580369735-photo_hdnndm.webp",
+  },
+  {
+    name: "Dr. Spina Luna Biswas",
+    email: "spina.luna@gmail.com",
+    specialty: "Teeth Cleaning",
+    degree: "BDS FCPS (Oral & Maxillofacial Surgery)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717579957/doctor-portal/doctors/1717580106978-photo_h8x9po.webp",
+  },
+  {
+    name: "Dr. Masuma Nawar",
+    email: "masuma@gmail.com",
+    specialty: "Teeth Cleaning",
+    degree: "BDS  DDS , PGT (ShSMCH)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717580058/doctor-portal/doctors/1717580989655-photo_ocooll.webp",
+  },
+  {
+    name: "Dr. Md. Ahsan Habib",
+    email: "shreyas2011@yahoo.com",
+    specialty: "Oral Surgery",
+    degree: "BDS, MS (OMFS)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717580215/doctor-portal/doctors/1717580316352-photo_ylustm.jpg",
+  },
+  {
+    name: "Dr. Morshed Asadullah Sikder",
+    email: "drmorshed74@gmail.com",
+    specialty: "Oral Surgery",
+    degree: "BDS, MS (Maxillofacial Surgery)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717580265/doctor-portal/doctors/1717580747630-photo_wnpqrg.jpg",
+  },
+  {
+    name: "Dr. Munmun Nahar",
+    email: "mnsurovi@gmail.com",
+    specialty: "Oral Surgery",
+    degree: "BDS, MS (OMS)",
+    photo:
+      "https://res.cloudinary.com/dnatcmedm/image/upload/v1717580339/doctor-portal/doctors/1717581269315-photo_m8400y.png",
+  },
+];
 
 export default data;
--- src/v1/controllers/seeds.controllers.mjs
+++ src/v1/controllers/seeds.controllers.mjs
@@ -2,6 +2,8 @@ import asyncHandler from "express-async-handler";
 import { successResponse } from "../../helper/responseHandler.mjs";
 import appointmentModel from "../../models/appointment.model.mjs";
 import appointmentData from "../../../data/seeds.appointment.mjs";
+import doctosData from "../../../data/seeds.doctor.mjs";
+import doctorModel from "../../models/doctor.model.mjs";
 
 // seed appointment data
 export const seedsAppoinments = asyncHandler(async (req, res, next) => {
@@ -18,6 +20,25 @@ export const seedsAppoinments = asyncHandler(async (req, res, next) => {
     payload: {
       total: appointments.length,
       data: appointments,
+    },
+  });
+});
+
+// seed doctos data
+export const seedsDoctors = asyncHandler(async (req, res, next) => {
+  // delete all existing tags
+  await doctorModel.deleteMany({});
+
+  // insert seeds data
+  const doctos = await doctorModel.create(doctosData);
+
+  // response with success message
+  successResponse(res, {
+    statusCode: 200,
+    message: "Seeds appointment data added successfully.",
+    payload: {
+      total: doctos.length,
+      data: doctos,
     },
   });
 });
