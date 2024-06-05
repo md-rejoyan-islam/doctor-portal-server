771e0594f5b8ee8ba6483363e2df04fcb9b56880
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-05 21:29:15 +0600

    feedback routes setup

--- src/v1/routes/feedback.routes.mjs
+++ src/v1/routes/feedback.routes.mjs
@@ -7,10 +7,15 @@ import {
 } from "../controllers/feedback.controllers.mjs";
 import { isLoggedIn } from "../../middlewares/verify.mjs";
 import { authorization } from "../../middlewares/authorization.mjs";
+import { feedbackCreateValidator } from "../../middlewares/validators/file/feedback.validator.mjs";
+import runValidation from "../../middlewares/validators/validation.mjs";
 
 const feedbackRouter = express.Router();
 
-feedbackRouter.route("/").get(isLoggedIn, getAllFeedbacks).post(createFeedback);
+feedbackRouter
+  .route("/")
+  .get(isLoggedIn, getAllFeedbacks)
+  .post(feedbackCreateValidator, runValidation, createFeedback);
 
 feedbackRouter
   .route("/:id([0-9a-fA-F]{24})")
