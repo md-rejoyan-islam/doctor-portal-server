ed6210819340b060d1f3645fcf79bdcec033108c
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-05 22:00:44 +0600

    payment route setup

--- src/v1/routes/payment.routes.mjs
+++ src/v1/routes/payment.routes.mjs
@@ -4,10 +4,25 @@ import {
   createPaymentIntent,
   getAllPayments,
 } from "../controllers/payment.controllers.mjs";
+import { isLoggedIn } from "../../middlewares/verify.mjs";
+import {
+  paymentCreateValidator,
+  paymentIntentCreateValidator,
+} from "../../middlewares/validators/file/payment.validator.mjs";
+import runValidation from "../../middlewares/validators/validation.mjs";
 
 const paymentRouter = express.Router();
 paymentRouter.route("/").get(getAllPayments);
-paymentRouter.route("/pay").post(createPayment);
-paymentRouter.route("/create-payment-intent").post(createPaymentIntent);
+paymentRouter
+  .route("/pay")
+  .post(isLoggedIn, paymentCreateValidator, runValidation, createPayment);
+paymentRouter
+  .route("/create-payment-intent")
+  .post(
+    isLoggedIn,
+    paymentIntentCreateValidator,
+    runValidation,
+    createPaymentIntent
+  );
 
 export default paymentRouter;
