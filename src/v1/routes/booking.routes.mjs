a293e0eb1c5bf70151a71ffc3a77fc5cfb02d480
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-05 19:58:36 +0600

    booking routes check

--- src/v1/routes/booking.routes.mjs
+++ src/v1/routes/booking.routes.mjs
@@ -7,18 +7,20 @@ import {
   updateBookingById,
 } from "../controllers/booking.controllers.mjs";
 import { isLoggedIn } from "../../middlewares/verify.mjs";
+import { bookingCreateValidator } from "../../middlewares/validators/file/booking.validatr.mjs";
+import runValidation from "../../middlewares/validators/validation.mjs";
 
 const bookingRouter = express.Router();
 
 bookingRouter
   .route("/")
   .get(isLoggedIn, getAllBookings)
-  .post(isLoggedIn, createBooking);
+  .post(isLoggedIn, bookingCreateValidator, runValidation, createBooking);
 
 bookingRouter
   .route("/:id([0-9a-fA-F]{24})")
-  .get(getBookingById)
-  .patch(updateBookingById)
-  .delete(deleteBookingById);
+  .get(isLoggedIn, getBookingById)
+  .patch(isLoggedIn, updateBookingById)
+  .delete(isLoggedIn, deleteBookingById);
 
 export default bookingRouter;
